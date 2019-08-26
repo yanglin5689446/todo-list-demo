b0f4e6a67f5267b7aa4c54fb2904f12b44d4c629
Yang Lin <yanglin5689446@gmail.com>
2019-08-26 23:07:52 +0800

    Build app architecture

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const App = () => (
   <div>
-    Hello, todo list!
+    <nav className="navbar navbar-dark bg-primary  navbar-expand-lg">
+      <a className="navbar-brand" href="#">Yang's todo-list</a>
+    </nav>
+    <div className="container py-3">
+      <div className="search-and-filter">
+        search and filter
+      </div>
+
+      <div className="add-task">
+        add-task
+      </div>
+
+      <div className='todo-list'>
+        todo list
+      </div>
+    </div>
   </div>
 )
 
