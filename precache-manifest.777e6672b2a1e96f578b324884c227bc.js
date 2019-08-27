576c0c1895c639dd713d0d915f294c2ce7b753c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 16:22:42 +0000

    Deploy yanglin5689446/todo-list-demo to github.com/yanglin5689446/todo-list-demo.git:gh-pages

---
+++ precache-manifest.777e6672b2a1e96f578b324884c227bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be81af315a0bfaa86a5226ce02e8300",
+    "url": "/todo-list-demo/index.html"
+  },
+  {
+    "revision": "8a0972b33f2dcc31a060",
+    "url": "/todo-list-demo/static/css/main.47d67f0e.chunk.css"
+  },
+  {
+    "revision": "87a9fd09c9da95237f27",
+    "url": "/todo-list-demo/static/js/2.7b3be5aa.chunk.js"
+  },
+  {
+    "revision": "8a0972b33f2dcc31a060",
+    "url": "/todo-list-demo/static/js/main.dddd5af6.chunk.js"
+  },
+  {
+    "revision": "d82d21f46e7951b707b8",
+    "url": "/todo-list-demo/static/js/runtime~main.422febc3.js"
+  }
+]);
